fd225cfe115c5969de61e265bfecc398a9e990e0
fossand <fossand@amazon.com>
2020-04-20 13:51:55 +0100

    types-of-values

--- value.h
+++ value.h
@@ -3,7 +3,33 @@
 
 #include "common.h"
 
-typedef double Value;
+typedef enum {
+    VAL_BOOL,
+    VAL_NIL,
+    VAL_NUMBER
+} ValueType; // 4 bytes
+
+typedef struct { 
+    ValueType type; // 4 bytes
+    union {
+        bool boolean;
+        double number;
+    } as; // union size is based on longest field i.e. double - has 8 bytes 
+} Value; // total of 16 bytes in a 64 bit machine, with 4 bytes of padding
+
+// Macros for creating Value
+#define BOOL_VAL(value)  ((Value){ VAL_BOOL, { .boolean = value }})
+#define NIL_VAL          ((Value){ VAL_NIL,  { .number = 0}})
+#define NUMBER_VAL(value)((Value){ VAL_NUMBER, { .number = value}})
+
+// returns actual value
+#define AS_BOOL(value) ((value).as.boolean)
+#define AS_NUMBER(value)((value).as.number)
+
+// Check Value type
+#define IS_BOOL(value) ((value).type == VAL_BOOL)
+#define IS_NIL(value)  ((value).type == VAL_NIL)
+#define IS_NUMBER(value) ((value).type == VAL_NUMBER)
 
 typedef struct {
     int capacity;
@@ -11,6 +37,7 @@ typedef struct {
     Value* values;
 } ValueArray;
 
+bool valuesEqual(Value a, Value b);
 void initValueArray(ValueArray* array);
 void writeValueArray(ValueArray* array, Value value);
 void freeValueArray(ValueArray* array);
--- vm.c
+++ vm.c
@@ -1,4 +1,5 @@
 #include <stdio.h> 
+#include <stdarg.h>
 
 #include "common.h"
 #include "compiler.h"
@@ -9,6 +10,19 @@ VM vm;
 
 static void resetStack() {
     vm.stackTop = vm.stack;
+}
+
+static void runtimeError(const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end(args);
+    fputs("\n", stderr);
+
+    size_t instruction = vm.ip - vm.chunk->code - 1;
+    int line = vm.chunk->lines[instruction];
+    fprintf(stderr, "[line %d] in script\n", line);
+    resetStack();
 }
 
 void initVM() {
@@ -29,29 +43,46 @@ Value pop() {
     return *vm.stackTop;
 }
 
+static Value peek(int distance) {
+    return vm.stackTop[-1 - distance];
+}
+
+static bool isFalsey(Value value) {
+    return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
+
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+    
 // https://pzemtsov.github.io/2014/05/05/do-macro.html
-#define BINARY_OP(op) \
+// valueType is a macro
+#define BINARY_OP(valueType, op) \
     do { \
-        double a = pop(); \
-        double b = pop(); \
-        push(a op b); \
+        if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) { \
+            runtimeError("Operands must be numbers."); \
+            return INTERPRET_RUNTIME_ERROR; \
+        } \
+        \
+        double b = AS_NUMBER(pop()); \
+        double a = AS_NUMBER(pop()); \
+        push(valueType(a op b)); \
     } while (false)
 
+    
     for (;;) {
 
-    #ifdef DEBUG_TRACE_EXECUTION
-        printf("          ");
-        for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
-            printf("[ ");
-            printValue(*slot);
-            printf(" ]");
-        }
-        printf("\n");
-        disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
-    #endif
+        #ifdef DEBUG_TRACE_EXECUTION
+            printf("          ");
+            for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+                printf("[ ");
+                printValue(*slot);
+                printf(" ]");
+            }
+            printf("\n");
+            disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+        #endif
 
         uint8_t instruction;
 
@@ -61,12 +92,30 @@ static InterpretResult run() {
                 push(constant);
                 break;
             }
-            case OP_ADD: BINARY_OP(+); break;
-            case OP_SUBTRACT: BINARY_OP(-); break;
-            case OP_MULTIPLY: BINARY_OP(*); break;
-            case OP_DIVIDE: BINARY_OP(/); break;
+            case OP_NIL: push(NIL_VAL); break;
+            case OP_TRUE: push(BOOL_VAL(true)); break;
+            case OP_FALSE: push(BOOL_VAL(false)); break;
+            case OP_EQUAL: {
+                Value b = pop();
+                Value a = pop();
+                push(BOOL_VAL(valuesEqual(a, b)));
+                break;
+            }
+            case OP_GREATER:  BINARY_OP(BOOL_VAL, >); break;
+            case OP_LESS:     BINARY_OP(BOOL_VAL, <); break;
+            case OP_ADD:      BINARY_OP(NUMBER_VAL, +); break;
+            case OP_SUBTRACT: BINARY_OP(NUMBER_VAL, -); break;
+            case OP_MULTIPLY: BINARY_OP(NUMBER_VAL, *); break;
+            case OP_DIVIDE:   BINARY_OP(NUMBER_VAL, /); break;
+            case OP_NOT:
+                push(BOOL_VAL(isFalsey(pop())));
+                break;
             case OP_NEGATE:
-                push(-pop());
+                if (!IS_NUMBER(peek(0))) {
+                    runtimeError("Operand must be a number.");
+                    return INTERPRET_RUNTIME_ERROR;
+                }
+                push(NUMBER_VAL(-AS_NUMBER(pop())));
                 break;
             case OP_RETURN: {
                 printValue(pop());
